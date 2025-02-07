7ae2cd50d492061788f87deb4a479136e23d6140
TheElo <elochai2@gmail.com>
2025-02-07 08:22:45 +0100

    test

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+populate_db_with_examples = False
+
+
+
+print("Create Database")
+# Check if it already exist
+
+# if so skip db creation
+
+# optionals population with examples
